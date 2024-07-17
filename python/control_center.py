3bc6677b832b2910cbb49142fd938f72d38bd10e
XHC <xuan-huy.cao@outlook.com>
2024-07-17 14:53:45 +0200

    add comments

--- python/control_center.py
+++ python/control_center.py
@@ -11,14 +11,17 @@ class ControlCenter:
     def add_elevator(self, elevator):
         self.elevators.append(elevator)
 
+    # Function to call an elevator to a floor
     async def call(self, floor, direction):
         elevator = self.get_elevator(floor, direction)
         print(elevator.id)
         await elevator.add_new_destination(floor)
 
+    # Function to add a new destination to an elevator when passengers are inside
     async def goto(self, elevator_id, floor):
         await self.elevators[elevator_id].add_new_destination(floor)
 
+    # Function to get the elevator that will respond to a call
     def get_elevator(self, floor, direction):
         # First, prioritize elevators that are idle
         distance = {i:abs(elevator.current_floor - floor) for i, elevator in enumerate(self.elevators) if elevator.state == Elevator.State.IDLE}
@@ -34,6 +37,9 @@ class ControlCenter:
         distance = {i:abs(elevator.current_floor - floor) for i, elevator in enumerate(self.elevators)}
         return self.elevators[min(distance, key = distance.get)]
     
+    # Function to parse the command received from the command board
+    # call <floor> <up|down>
+    # goto <elevator_id> <floor>
     def parse_command(self, raw_command):
         command_args = raw_command.decode('UTF-8').split()
         print(command_args)
@@ -47,6 +53,7 @@ class ControlCenter:
         else:
             return None
     
+    # Function to process the command received from the command board
     async def process_command(self, command):
         if isinstance(command, CallCommand):
             print("call")
@@ -57,6 +64,7 @@ class ControlCenter:
         else:
             print("Invalid command")
 
+    # Function to run the command board
     async def run_command_board(self):
         stdin, stdout = await aioconsole.get_standard_streams()
         async for line in stdin:
@@ -64,6 +72,7 @@ class ControlCenter:
             await self.process_command(command)
             stdout.write(line)
     
+    # Function to run the control center
     async def run(self):
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.run_command_board())
--- python/elevator.py
+++ python/elevator.py
@@ -23,17 +23,28 @@ class Elevator:
         self.new_destination_event = asyncio.Event()
         self.lock = asyncio.Lock()
     
+    def __str__(self):
+        dir = 'UP' if self.current_direction == Direction.UP else 'DOWN'
+        door = 'OPEN' if self.state == Elevator.State.STOPPED else 'CLOSED'
+        return f'{self.id}|floor:{self.current_floor}|dir:{dir}|door:{door}'
+    
+    # Function to fetch the newly added destination
     async def fetch_new_destination(self):
         while True:
+            # Wait for the new destination event to be set
             await self.new_destination_event.wait()
             async with self.lock:
                 self.set_next_destination()
                 self.new_destination_event.clear()
     
+    # Function to set the next destination
     def set_next_destination(self):
+        # If the current destination is not None, add it to back the set of destination floors for later processing
         if self.current_destination != None:
             self.destination_floors.add(self.current_destination)
 
+        # If the elevator is going up, get the next upper floor as the destination
+        # If there are no upper floors, change the direction to down and get the next lower floor as the destination
         if self.current_direction == Direction.UP:
             upper_floors = [floor for floor in self.destination_floors if floor > self.current_floor]
             if upper_floors:
@@ -43,6 +54,8 @@ class Elevator:
                 lower_floors = [floor for floor in self.destination_floors if floor < self.current_floor]
                 if lower_floors:
                     self.current_destination = max(lower_floors)
+        # If the elevator is going down, get the next lower floor as the destination
+        # If there are no lower floors, change the direction to up and get the next upper floor as the destination
         else:
             lower_floors = [floor for floor in self.destination_floors if floor < self.current_floor]
             if lower_floors:
@@ -54,30 +67,24 @@ class Elevator:
                     self.current_destination = min(upper_floors)
         
         self.destination_floors.remove(self.current_destination)
-    
+
+    # Function to add a new destination to the set of destination floors and trigger the new destination event
     async def add_new_destination(self, floor):
         async with self.lock:
             self.destination_floors.add(floor)
             self.new_destination_event.set()
 
-    async def stop(self):
-        print(f'--Elevator {self.id} has arrived at floor {self.current_floor}')
-        print(f'--The door of {self.id} will remain open for 5 seconds')
-        self.state = Elevator.State.STOPPED
-        # self.destination_floors.remove(self.current_destination)
-        self.current_destination = None
+    # Function to set the elevator to idle state
+    async def idle(self):
+        print(f'Elevator {self.id} is idle')
+        self.state = Elevator.State.IDLE
+        await self.new_destination_event.wait()
+        await self.move()
 
-        await asyncio.sleep(5)
-        print(f'--The door of {self.id} is closing')
-        if self.destination_floors:
-            self.set_next_destination()
-            await self.move()
-        else:
-            await self.idle()
-
+    # Function to set the elevator to moving state
+    # The elevator will move to the next floor until it reaches the current destination
     async def move(self):
         self.state = Elevator.State.MOVING
-        # TODO: Implement the move method
         while self.current_floor != self.current_destination:
             async with self.lock:
                 if self.current_floor < self.current_destination:
@@ -88,12 +95,23 @@ class Elevator:
             await asyncio.sleep(1 / self.speed)
         await self.stop()
 
-    async def idle(self):
-        print(f'Elevator {self.id} is idle')
-        self.state = Elevator.State.IDLE
-        await self.new_destination_event.wait()
-        await self.move()
+    # Function to set the elevator to stop state
+    # The elevator will remain stopped for 5 seconds before moving to the next destination (if any)
+    async def stop(self):
+        print(f'--Elevator {self.id} has arrived at floor {self.current_floor}')
+        print(f'--The door of {self.id} will remain open for 5 seconds')
+        self.state = Elevator.State.STOPPED
+        self.current_destination = None
 
+        await asyncio.sleep(5)
+        print(f'--The door of {self.id} is closing')
+        if self.destination_floors:
+            self.set_next_destination()
+            await self.move()
+        else:
+            await self.idle()
+
+    # Function to run the elevator
     async def run(self):
         async with asyncio.TaskGroup() as tg:
             tg.create_task(self.fetch_new_destination())
